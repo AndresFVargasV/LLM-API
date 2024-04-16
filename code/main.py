0bd42340bbe036cf7e7e473d4eb3599f052cd00a
AndresFVargasV <afvasquez@uninorte.edu.co>
2024-04-16 16:59:10 -0500

    prueba

--- code/main.py
+++ code/main.py
@@ -9,9 +9,21 @@ app = FastAPI()
 class Item(BaseModel):
     carrera: str
     temaEstudio: str
-    horaInicial_tiempoLibre: str
-    horaFinal_tiempoLibre: str
-    diasEstudio: str
+    horaInicial_tiempoLibreL: str
+    horaFinal_tiempoLibreL: str
+    horaInicial_tiempoLibreM: str
+    horaFinal_tiempoLibreM: str
+    horaInicial_tiempoLibreW: str
+    horaFinal_tiempoLibreW: str
+    horaInicial_tiempoLibreJ: str
+    horaFinal_tiempoLibreJ: str
+    horaInicial_tiempoLibreV: str
+    horaFinal_tiempoLibreV: str
+    horaInicial_tiempoLibreS: str
+    horaFinal_tiempoLibreS: str
+    horaInicial_tiempoLibreD: str
+    horaFinal_tiempoLibreD: str
+
 
 @app.get("/")
 async def root():
@@ -19,6 +31,7 @@ async def root():
 
 @app.post("/study-plan")
 async def study_plan(item: Item):
+    print(item)
     response = llm.response(item.carrera, item.temaEstudio, item.horaInicial_tiempoLibre, item.horaFinal_tiempoLibre, item.diasEstudio)
     return response
 
